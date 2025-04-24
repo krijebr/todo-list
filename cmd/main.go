99dd915f031c72388e40415cbdf1ec00bd59a548
krijebr <krijebr123@gmail.com>
2025-04-24 17:03:46 +0300

    update main pkg

--- cmd/main.go
+++ cmd/main.go
@@ -7,41 +7,25 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/krijebr/todo-list/internal/config"
 	v1 "github.com/krijebr/todo-list/internal/controller/http/v1"
 	"github.com/krijebr/todo-list/internal/repo"
 	"github.com/krijebr/todo-list/internal/usecase"
 	_ "github.com/lib/pq"
 )
 
-const port int = 8080
-
-const (
-	host     = "localhost"
-	portdb   = 5432
-	username = "postgres"
-	password = "mysecretpassword"
-	dbname   = "postgres"
-)
-
-type ConfigPostgres struct {
-	host     string
-	portdb   int
-	username string
-	password string
-	dbname   string
-}
+const confpath string = "../config/config.json"
 
 func main() {
 	log.Println("starting app")
 
-	configpostgres := ConfigPostgres{
-		host:     host,
-		portdb:   portdb,
-		username: username,
-		password: password,
-		dbname:   dbname,
+	cfg, err := config.InitConfigFromJson(confpath)
+	if err != nil {
+		log.Println("Ошибка инициализации", err)
+		return
 	}
-	db, err := initDB(configpostgres)
+
+	db, err := initDB(cfg)
 	if err != nil {
 		log.Println("Ошибка инициализации базы данных", err)
 		return
@@ -51,7 +35,7 @@ func main() {
 	uc := usecase.NewTaskUseCase(repoPostgres)
 	r := v1.CreateRouter(uc)
 
-	addr := ":" + strconv.Itoa(port)
+	addr := ":" + strconv.Itoa(cfg.HttpServer.Port)
 	log.Printf("starting http server on port %s\n", addr)
 	err = http.ListenAndServe(addr, r)
 	if err != nil {
@@ -59,8 +43,8 @@ func main() {
 	}
 }
 
-func initDB(cp ConfigPostgres) (*sql.DB, error) {
-	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", cp.host, cp.portdb, cp.username, cp.password, cp.dbname)
+func initDB(cfg *config.Config) (*sql.DB, error) {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.UserName, cfg.Postgres.Password, cfg.Postgres.DBName)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
