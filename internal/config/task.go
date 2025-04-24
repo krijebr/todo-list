481207b65688ce501b338255ae0ee1e4d55ec92c
krijebr <krijebr123@gmail.com>
2025-04-24 15:55:09 +0300

    update config pkg

--- internal/config/task.go
+++ internal/config/task.go
@@ -1,16 +1,18 @@
 package config
 
-type ConfigPostgres struct {
-	Host     string `json:"host"`
-	PortDB   int    `json:"port_db"`
-	UserName string `json:"user_name"`
-	Password string `json:"password"`
-	DBName   string `json:"db_name"`
-}
-type ConfigRouter struct {
-	PortR int `json:"port_r"`
-}
-type Config struct {
-	ConfigPostgres
-	ConfigRouter
-}
+type (
+	Postgres struct {
+		Host     string `json:"host"`
+		Port     int    `json:"port"`
+		UserName string `json:"user_name"`
+		Password string `json:"password"`
+		DBName   string `json:"db_name"`
+	}
+	HttpServer struct {
+		Port int `json:"port"`
+	}
+	Config struct {
+		Postgres   Postgres   `json:"postgres"`
+		HttpServer HttpServer `json:"http_server"`
+	}
+)
